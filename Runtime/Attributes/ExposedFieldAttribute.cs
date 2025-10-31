1140e40228d57e3119537b5f98abc7253339eb5b
SunShubin <canghuolong@163.com>
2025-10-31 23:17:03 +0800

    修改编辑器

---
+++ Runtime/Attributes/ExposedFieldAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ActionSequence
+{
+    public class ExposedFieldAttribute : Attribute
+    {
+        
+    }
+}
---
+++ UnityComponents/UnityData/LegacyAnimationData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace ActionSequence
+{
+    [Serializable]
+    public class LegacyAnimationData : AActionClipData<LegacyAnimationAction>
+    {
+        [ExposedField]
+        public Animation animation;
+        
+        [ExposedField]
+        public string clipName;
+
+    }
+    public class LegacyAnimationAction : IPoolAction<LegacyAnimationData>,IStartAction,IUpdateAction,ICompleteAction
+    {
+        private LegacyAnimationData _clipData;
+        public void SetParams(object param)
+        {
+            _clipData = param as LegacyAnimationData;
+        }
+
+        public void Reset()
+        {
+            
+        }
+        
+        public void Start()
+        {
+            _clipData.animation.Play(_clipData.clipName);
+        }
+
+        public void Update(float localTime, float totalTime)
+        {
+            _clipData.animation[_clipData.clipName].normalizedTime = localTime/totalTime;
+            _clipData.animation.Sample();
+        }
+        
+        public void Complete()
+        {
+            _clipData.animation[_clipData.clipName].enabled = false;
+        }
+
+        public bool IsFromPool { get; set; }
+
+     
+    }
+}
